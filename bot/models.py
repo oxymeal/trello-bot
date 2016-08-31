1e8a42987e305664c8094c42068218eba84002ac
Oleg Yamnikov <bgeyts667@gmail.com>
2016-08-31 16:50:56 +0500

    Add BoardHook model

--- bot/models.py
+++ bot/models.py
@@ -17,7 +17,12 @@ class Session(BaseModel):
     trello_token = peewee.CharField(null=True)
 
 
+class BoardHook(BaseModel):
+    session = peewee.ForeignKeyField(Session, related_name='hooks')
+    board_id = peewee.CharField()
+
+
 db.init(config.DB_FILE)
 
 if not os.path.isfile(config.DB_FILE):
-    db.create_tables([Session])
+    db.create_tables([Session, BoardHook])
