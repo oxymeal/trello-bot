9bab7971e75c72bc3d35c9ab954bc80d4c341069
Oleg Yamnikov <bgeyts667@gmail.com>
2016-08-31 14:34:01 +0500

    Restructure trello.py requests api

--- bot/trello.py
+++ bot/trello.py
@@ -5,26 +5,43 @@ import requests
 TRELLO_API_URL = 'https://trello.com/1'
 
 
-class AuthError(Exception):
-    def __init__(self, session):
-        self.session = session
-        super().__init__("Request was denied; app key = {}, token = {}".format(
-            self.session.app.key, self.session.token))
-
-
-class UnknownStatusError(Exception):
-    def __init__(self, session, status_code, text):
+class TrelloError(Exception):
+    def __init__(self, session, status_code, url, text, desc="API call error"):
         self.session = session
         self.status_code = status_code
+        self.url = url
         self.text = text
 
-        if len(self.text) > 12:
-            display_text = self.text[:9] + '...'
+        maxlen = 255
+        if len(self.text) > maxlen:
+            display_text = self.text[:(maxlen-3)] + '...'
         else:
             display_text = self.text
 
-        super().__init__("Unknown response status {}: {}", self.status_code,
-                         display_text)
+        super().__init__("{desc}: {url} -> {status_code} {display_text}"
+                         .format(desc=desc, url=url,
+                                 status_code=status_code,
+                                 display_text=display_text))
+
+
+class CustomTrelloError(TrelloError):
+    status_code = 0
+    desc = ""
+
+    def __init__(self, session, url, text):
+        super().__init__(session, self.status_code, url, text, self.desc)
+
+class AuthError(CustomTrelloError):
+    status_code = 401
+    desc = "Request was denied"
+
+class NotFoundError(CustomTrelloError):
+    status_code = 404
+    desc = "URL not found"
+
+class RequestError(CustomTrelloError):
+    status_code = 400
+    desc = "Invalid data"
 
 
 class App:
@@ -51,30 +68,46 @@ class Session:
         self.app = app
         self.token = token
 
-    def _api_get(self, url, params=None):
+    def _api_request(self, method, url, params=None, data=None):
         if params is None: params = {}
 
         params['key'] = self.app.key
         params['token'] = self.token
 
-        r = requests.get(TRELLO_API_URL + url, params=params)
+        r = requests.request(method, TRELLO_API_URL + url, params=params, data=data)
 
+        if r.status_code == 400:
+            raise RequestError(self, url, r.text)
         if r.status_code == 401:
-            raise AuthError(self)
+            raise AuthError(self, url, r.text)
+        if r.status_code == 404:
+            raise NotFoundError(self, url, r.text)
         elif r.status_code != 200:
-            raise UnknownStatusError(self, r.status_code, r.text)
+            raise TrelloError(self, r.status_code, url, r.text)
 
         return r.json()
 
-    def boards(self):
-        boards_json = self._api_get('/member/me/boards')
-        return [Board(self, b['id'], b['name'], b['desc'], b['url'])
-                for b in boards_json]
+    def _api_get(self, url, *, params=None):
+        return self._api_request('get', url, params)
+
+    def _api_post(self, url, *, params=None, data=None):
+        return self._api_request('post', url, params, data)
+
+    def _api_put(self, url, *, params=None, data=None):
+        return self._api_request('put', url, params, data)
+
+    def _api_delete(self, url, *, params=None):
+        return self._api_request('delete', url, params)
 
     def me(self):
         me_json = self._api_get('/members/me')
         return Member(self, me_json['id'], me_json['username'],
                       me_json['fullName'], me_json['url'])
+
+    def boards(self):
+        boards_json = self._api_get('/member/me/boards')
+        return [Board(self, b['id'], b['name'], b['desc'], b['url'])
+                for b in boards_json]
 
 
 class Member:
