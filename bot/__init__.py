b38b10fca7de1202a63f9ed2d5dff7a18fa87576
Oleg Yamnikov <bgeyts667@gmail.com>
2016-09-01 15:15:46 +0500

    Add hook management commands and dialogs to the bot

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,12 +2,73 @@ import logging
 from typing import *
 
 from bot import models, trello, messages
-from bot.base_bot import BaseBot, Context
+from bot.base_bot import BaseBot, Context, Dialog
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
+
+class AddHookDialog(Dialog):
+
+    def __init__(self, boards):
+        self.boards = boards
+        self.board_options = [ b.name for b in self.boards ]
+        self.board_map = { b.name: b for b in self.boards }
+
+        super().__init__()
+
+    def step1(self, ctx: Context):
+        try:
+            board = self.board_map[ctx.text]
+        except KeyError:
+            ctx.send_message(messages.NOTIFY_NOBOARD)
+            return False
+
+        (hook, created) = models.BoardHook.get_or_create(session=ctx.session, board_id=board.id)
+        if not created:
+            ctx.send_message(messages.NOTIFY_ALREADY)
+            return True
+
+        ctx.send_message(messages.NOTIFY_SUCCESS)
+        return True
+
+    step1_message = messages.NOTIFY_DLG_MSG
+
+    @property
+    def step1_options(self):
+        return self.board_options
+
+    def cancel(self, ctx: Context):
+        ctx.send_message(messages.NOTIFY_CANCELLED)
+        return True
+
+class ForgetHookDialog(Dialog):
+    def __init__(self, hook_map):
+        self.hook_map = hook_map
+        self.hook_options = list(self.hook_map.keys())
+        super().__init__()
+
+    def step1(self, ctx: Context):
+        try:
+            hook = self.hook_map[ctx.text]
+        except KeyError:
+            ctx.send_message(messages.FORGET_NOBOARD)
+            return False
+
+        hook.delete_instance()
+        ctx.send_message(messages.FORGET_SUCCESS)
+        return True
+
+    step1_message = messages.FORGET_DLG_MSG
+
+    @property
+    def step1_options(self):
+        return self.hook_options
+
+    def cancel(self, ctx: Context):
+        ctx.send_message(messages.FORGET_CANCELLED)
+        return True
 
 def require_auth(fn):
     def wrapper(self, ctx, *args, **kwargs):
@@ -71,3 +132,36 @@ class TrelloBot(BaseBot):
         ctx.session.trello_token = None
         ctx.session.save()
         ctx.send_message(messages.UNAUTH_SUCCESS)
+
+    @require_auth
+    def cmd_notify(self, ctx: Context):
+        boards = ctx.trello_session.members.me().boards(filter='open')
+        ctx.start_dialog(AddHookDialog(boards))
+
+    @require_auth
+    def cmd_list(self, ctx: Context):
+        hooks = ctx.session.hooks.execute()
+
+        hooks_msgs = []
+        for h in hooks:
+            try:
+                b = ctx.trello_session.boards.get(h.board_id)
+                bname = b.name
+            except trello.NotFoundError:
+                h.delete()
+
+            msg = messages.LIST_ITEM.format(board=bname)
+            hooks_msgs.append(msg)
+
+        msg = messages.LIST.format(list='\n'.join(hooks_msgs))
+        ctx.send_message(msg)
+
+    @require_auth
+    def cmd_forget(self, ctx: Context):
+        hooks = ctx.session.hooks.execute()
+        hook_map = {}
+        for h in hooks:
+            b = ctx.trello_session.boards.get(h.board_id)
+            hook_map[b.name] = h
+
+        ctx.start_dialog(ForgetHookDialog(hook_map))
